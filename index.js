77e97409348c7fa518548e649266f6fc223c3a02
Mohamad Ayyash <mkayyash@gmail.com>
2023-02-25 19:14:21 -0500

    Updates

--- index.js
+++ index.js
@@ -27,7 +27,31 @@ app.get("/generate/:formUrl", (req, res) => {
     });
 });
 
-const port = process.env.PORT || 3005;
-app.listen(port, "0.0.0.0", () => {
-  console.log(`Server listening at http://localhost:${port}`);
+const { MessagingResponse } = require('twilio').twiml;
+
+const goodBoyUrl = 'https://images.unsplash.com/photo-1518717758536-85ae29035b6d?'
+  + 'ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80';
+
+app.post("/", (req, res) => {
+  const { body } = req;
+
+  let message;
+
+  if (body.NumMedia > 0) {
+    message = new MessagingResponse().message("Thanks for the image! Here's one for you!");
+    message.media(goodBoyUrl);
+  } else {
+    message = new MessagingResponse().message('Send us an image!');
+  }
+
+  res.set('Content-Type', 'text/xml');
+  res.send(message.toString()).status(200);
 });
+
+const port = process.env.PORT || 3005;
+const host = process.env.HOST || "0.0.0.0";
+app.listen(port, host, () => {
+  console.log(`Server listening at ${host}:${port}`);
+});
+
+module.exports = app;
