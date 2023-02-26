9ccbbebd734802ce796dcbba9c64d5ddf4126ea9
Mohamad Ayyash <mkayyash@gmail.com>
2023-02-25 19:05:24 -0500

    Updates:

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
-  require("dotenv").config();
-  const express = require("express");
-  const app = express();
-  const axios = require("axios");
-  const cheerio = require("cheerio");
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-  const formUrl = "https://forms.gle/j6BzcWNmqaHpmmvZ6";
+app.get("/generate/:formUrl", (req, res) => {
+  axios
+    .get(req.params.formUrl)
+    .then((response) => {
+      const $ = cheerio.load(response.data);
+      const ogTitle = $('meta[property="og:title"]').attr("content");
+      const ogDescription = $('meta[property="og:description"]').attr(
+        "content"
+      );
+      const result = `📖  ${ogTitle} 📖
 
-app.get("/generate", (req, res) => {
-    axios
-      .get(formUrl)
-      .then((response) => {
-        const $ = cheerio.load(response.data);
-        const ogTitle = $('meta[property="og:title"]').attr("content");
-        const ogDescription = $('meta[property="og:description"]').attr(
-          "content"
-        );
-        const result =
-        `📖  ${ogTitle} 📖
-
-        ${formUrl}
+        ${req.params.formUrl}
 
 
       🖋   ${ogDescription.replace(/(\r\n|\n|\r)/gm, " ")} 🖋 `;
-        console.log(result);
-        res.send(result);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  });
+      console.log(result);
+      res.send(result);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
 
-  const port = process.env.PORT | 3005;
-  app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
-  });
+const port = process.env.PORT || 3005;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
